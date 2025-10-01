52254fc2ec27ddba12c2c7d0c70515efb102a002
logoes0 <logpanja2k4@gmail.com>
2025-10-01 18:22:47 +0530

    updated code

--- home/code/default.nix
+++ home/code/default.nix
@@ -1,38 +1,48 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}: {
+{ pkgs, config, lib, ... }: {
   programs.vscode = {
     enable = true;
     extensions = with pkgs.vscode-extensions; [
       # Python
       ms-python.python
       ms-python.vscode-pylance
-
       # C/C++
       ms-vscode.cpptools
-
       # Go
       golang.go
+      # Nix
+      jnoortheen.nix-ide
+      # Utility
+      mkhl.direnv  # For auto-loading shell.nix environments
     ];
+
+    userSettings = {
+      # Nix IDE settings
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "${pkgs.nixd}/bin/nixd";
+      "nix.formatterPath" = "${pkgs.alejandra}/bin/alejandra";
+      "[nix]" = {
+        "editor.tabSize" = 2;
+        "editor.formatOnSave" = true;
+      };
+    };
   };
 
-  # Optional: Install language tooling
+  # Language tooling
   home.packages = with pkgs; [
     # Python
     python3
     python3Packages.pip
-
     # C++
     gcc
     clang-tools
     cmake
-
     # Go
     go
     gopls
     delve
+    # Nix
+    nixd         # Nix language server
+    alejandra    # Nix formatter
+    nil          # Alternative Nix language server (optional)
   ];
 }
--- home/hypr/hyprlock.nix
+++ home/hypr/hyprlock.nix
@@ -2,20 +2,8 @@
   programs.hyprlock = {
     enable = true;
     settings = {
-      "$font" = "Monospace";
-
       general = {
         hide_cursor = false;
-      };
-
-      animations = {
-        enabled = true;
-        bezier = "linear, 1, 1, 0, 0";
-        animation = [
-          "fadeIn, 1, 5, linear"
-          "fadeOut, 1, 5, linear"
-          "inputFieldDots, 1, 2, linear"
-        ];
       };
 
       background = [
@@ -29,23 +17,17 @@
       input-field = [
         {
           monitor = "";
-          size = "20%, 5%";
+          size = "300, 50";
           outline_thickness = 3;
-          inner_color = "rgba(0, 0, 0, 0.0)"; # no fill
-
-          outer_color = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-          check_color = "rgba(00ff99ee) rgba(ff6633ee) 120deg";
-          fail_color = "rgba(ff6633ee) rgba(ff0066ee) 40deg";
-
+          inner_color = "rgba(0, 0, 0, 0.0)";
+          outer_color = "rgba(51, 204, 255, 0.93)";
+          check_color = "rgba(0, 255, 153, 0.93)";
+          fail_color = "rgba(255, 102, 51, 0.93)";
           font_color = "rgb(143, 143, 143)";
           fade_on_empty = false;
           rounding = 15;
-
-          font_family = "$font";
           placeholder_text = "Input password...";
-          fail_text = "$PAMFAIL";
           dots_spacing = 0.3;
-
           position = "0, -20";
           halign = "center";
           valign = "center";
@@ -58,20 +40,15 @@
           monitor = "";
           text = "$TIME";
           font_size = 90;
-          font_family = "$font";
-
           position = "-30, 0";
           halign = "right";
           valign = "top";
         }
-
         # DATE
         {
           monitor = "";
-          text = ''cmd[update:60000] date +"%A, %d %B %Y"''; # update every 60 seconds
+          text = ''cmd[update:60000] date +"%A, %d %B %Y"'';
           font_size = 25;
-          font_family = "$font";
-
           position = "-30, -150";
           halign = "right";
           valign = "top";
@@ -80,3 +57,86 @@
     };
   };
 }
+
+# {
+#   programs.hyprlock = {
+#     enable = true;
+#     settings = {
+#       "$font" = "Monospace";
+#
+#       general = {
+#         hide_cursor = false;
+#       };
+#
+#       animations = {
+#         enabled = true;
+#         bezier = "linear, 1, 1, 0, 0";
+#         animation = [
+#           "fadeIn, 1, 5, linear"
+#           "fadeOut, 1, 5, linear"
+#           "inputFieldDots, 1, 2, linear"
+#         ];
+#       };
+#
+#       background = [
+#         {
+#           monitor = "";
+#           path = "screenshot";
+#           blur_passes = 3;
+#         }
+#       ];
+#
+#       input-field = [
+#         {
+#           monitor = "";
+#           size = "20%, 5%";
+#           outline_thickness = 3;
+#           inner_color = "rgba(0, 0, 0, 0.0)"; # no fill
+#
+#           outer_color = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+#           check_color = "rgba(00ff99ee) rgba(ff6633ee) 120deg";
+#           fail_color = "rgba(ff6633ee) rgba(ff0066ee) 40deg";
+#
+#           font_color = "rgb(143, 143, 143)";
+#           fade_on_empty = false;
+#           rounding = 15;
+#
+#           font_family = "$font";
+#           placeholder_text = "Input password...";
+#           fail_text = "$PAMFAIL";
+#           dots_spacing = 0.3;
+#
+#           position = "0, -20";
+#           halign = "center";
+#           valign = "center";
+#         }
+#       ];
+#
+#       label = [
+#         # TIME
+#         {
+#           monitor = "";
+#           text = "$TIME";
+#           font_size = 90;
+#           font_family = "$font";
+#
+#           position = "-30, 0";
+#           halign = "right";
+#           valign = "top";
+#         }
+#
+#         # DATE
+#         {
+#           monitor = "";
+#           text = ''cmd[update:60000] date +"%A, %d %B %Y"''; # update every 60 seconds
+#           font_size = 25;
+#           font_family = "$font";
+#
+#           position = "-30, -150";
+#           halign = "right";
+#           valign = "top";
+#         }
+#       ];
+#     };
+#   };
+# }
